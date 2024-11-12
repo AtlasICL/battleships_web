71aa2a9144150392c8696588af1d5b4a944d1f47
Emre Acarsoy <ea616@exeter.ac.uk>
2024-11-12 16:36:07 +0000

    progress

--- components.py
+++ components.py
@@ -6,21 +6,33 @@ def initialise_board(size: int =10) -> list[list]:
 
 def create_battleships(filename: str ="battleships.txt") -> dict[str, int]:
     battleships: dict = {} 
-    with open(filename, 'r') as istream:
-        for line in istream:
+    with open(filename, 'r') as ifstream:
+        for line in ifstream:
             ship_name, ship_size = line.strip().split(":")
             battleships[ship_name] = int(ship_size)
     return battleships
 
 def place_battleships(board: list[list], ships: dict[str, int]) -> list[list]:
-    pass
+    assert len(ships) <= len(board), "TOO MANY SHIPS"
+    for ship_size in ships.values():
+        assert ship_size <= len(board), "SHIP TOO BIG"
+    
+    row: int = 0
+    for ship_name, ship_size in ships.items():
+        for x in range(ship_size):
+            board[row][x] = ship_name
+        row += 1
+
+    print_board(board)
+
+
     
 
 def main():
     board: list[list] = initialise_board()
     ships: dict[str: int] = create_battleships()
     board = place_battleships(board, ships)
-    print_board(board)
+    
 
 if __name__ == "__main__":
     main()
