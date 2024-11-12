918af0f818b35f5a2c2ce4c608af0fc2429e3817
Emre Acarsoy <ea616@exeter.ac.uk>
2024-11-12 16:17:29 +0000

    implemented initialise_board and create_battleships

--- components.py
+++ components.py
@@ -1,9 +1,28 @@
-def initialise_board(size :int =10) -> list[list]:
-    pass
+from utils import print_board
+
+def initialise_board(size: int =10) -> list[list]:
+    empty_board = [[None for _ in range(size)] for _ in range(size)]
+    return empty_board
 
 def create_battleships(filename: str ="battleships.txt") -> dict[str, int]:
-    pass
+    battleships: dict = {} 
+    with open(filename, 'r') as istream:
+        for line in istream:
+            ship_name, ship_size = line.strip().split(":")
+            battleships[ship_name] = int(ship_size)
+    return battleships
 
-def place_battleships(board: list[list], ships: dict[int, str]) -> list[list]:
+def place_battleships(board: list[list], ships: dict[str, int]) -> list[list]:
     pass
     
+
+def main():
+    board: list[list] = initialise_board()
+    ships: dict[str: int] = create_battleships()
+    board = place_battleships(board, ships)
+    print_board(board)
+
+if __name__ == "__main__":
+    main()
+
+
