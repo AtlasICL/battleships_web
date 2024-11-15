c26416900089dc91725460bf8186d12c2527a559
Emre Acarsoy <ea616@exeter.ac.uk>
2024-11-15 16:38:57 +0000

    basic cli functionality working

--- game_engine.py
+++ game_engine.py
@@ -1,8 +1,14 @@
+import components
+import debug
+
 def ship_exists(coordinates: tuple, board: list[list]) -> bool:
     y, x = coordinates
     if board[y][x] is not None:
         return True
     return False
+
+def display_welcome_message() -> None:
+    print("Welcome to Battleships game")
 
 def attack(coordinates: tuple, board: list[list], battleships: dict[str, int]) -> bool:
     x, y = coordinates
@@ -13,12 +19,20 @@ def attack(coordinates: tuple, board: list[list], battleships: dict[str, int]) -
     return hit_success
 
 def cli_coordinates_input() -> tuple:
-    x = input("Please enter x coordinate --> ")
-    y = input("Please enter y coordinate --> ")
+    x = int(input("Please enter x coordinate --> "))
+    y = int(input("Please enter y coordinate --> "))
     return (x, y)
 
 def simple_game_loop():
-    pass
+    display_welcome_message()
+    board = components.initialise_board(5)
+    ships = components.create_battleships()
+    board = components.place_battleships(board, ships)
+    while True:
+        debug.print_board(board)
+        (x, y) = cli_coordinates_input()
+        attack((x, y), board, ships)
+    
 
 if __name__ == "__main__":
     simple_game_loop()
