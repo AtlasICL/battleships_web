e4c1e2baaa7f0171cdbe8a0e7b5a85031e28a735
Emre Acarsoy <ea616@exeter.ac.uk>
2024-11-16 14:28:25 +0000

    mp progress

--- mp_game_engine.py
+++ mp_game_engine.py
@@ -1,4 +1,14 @@
-players: dict = {}
+players = {
+    "Emre": {
+        "board": [[None, None], [None, None]],
+        "ships": {"s1": 0, "s2": 0}
+    },
+    "Julia": {
+        "board": [['s1', 's1'], ['s2', None]],
+        "ships": {"s1": 2, "s2": 1}
+    }
+}
+
 
 def generate_attack() -> tuple:
     pass
